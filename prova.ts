4c0e4e72e78bbb5c7a0584e0aca7164de74af268
geno91 <andrea.domenico.genovese@gmail.com>
2022-02-22 12:55:44 +0100

    feat(prova): alessandro line modified

--- prova.ts
+++ prova.ts
@@ -9,5 +9,5 @@ t + g + v + l + d;
 const line = "hello";
 t + g + v + l + d;
 const branch="main-first, main-first-new-comment";
-const branch1="main-first, main-first-new-comment, andrea";
+const branch1="main-first, main-first-new-comment, andrea, alessandro";
 const branch2= "alessandro fai un rebase";
